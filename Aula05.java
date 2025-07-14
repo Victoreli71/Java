b60628135d6ba56be2898b067a9da41576cca8de
Victor H. E. França <123619380+Victoreli71@users.noreply.github.com>
2025-07-13 21:21:57 -0300

    Create Aula05.java

---
+++ Aula05.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class Aula05 {
+	public static void main(String[] args) {
+		ContaBanco p1 = new ContaBanco();
+		p1.setNumConta(111111);
+		p1.setDono("Victor França");
+		p1.estadoAtual();
+	}
+	
+
+}
