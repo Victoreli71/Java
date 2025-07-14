7a95efafc5278f3aa48227270db2aafb8ba19b29
Victor H. E. França <123619380+Victoreli71@users.noreply.github.com>
2025-07-13 21:16:24 -0300

    Create teste.java

---
+++ teste.java
@@ -0,0 +1,32 @@
+package exercicios;
+
+public class Teste {
+	private String marca;
+	private float oleo;
+	private String cor;
+	private boolean aberto;
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public float getOleo() {
+		return oleo;
+	}
+	public void setOleo(float oleo) {
+		this.oleo = oleo;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public boolean isAberto() {
+		return aberto;
+	}
+	public void setAberto(boolean aberto) {
+		this.aberto = aberto;
+	}
+}
