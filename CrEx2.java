fc1923a52315b93e84a6a3ff60afabc43c35e9c1
Victor H. E. França <123619380+Victoreli71@users.noreply.github.com>
2025-09-13 17:34:43 -0300

    Create CrEx2.java

---
+++ CrEx2.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		int idade;
+		idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade :"));
+		
+		if(idade >= 18) {
+			JOptionPane.showMessageDialog(null, "Maior de idade");
+		}
+		else
+		{
+			JOptionPane.showMessageDialog(null, "Menor de idade");
+		}
+
+	}
+
+}
